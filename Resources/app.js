7978adcf05ce7f3fd45a887e01b17a4480f6d123
Darren Cope <github@darren.cope.name>
2013-02-24 23:25:20 +0000

    initial commit

---
+++ Resources/app.js
@@ -0,0 +1,49 @@
+// All source code Copyright 2013 Cope Consultancy Services. All rights reserved
+
+
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+var forex = require('/cloud');
+forex.init();
+// create base root window
+//
+var win1 = Titanium.UI.createWindow({  
+    backgroundColor:'#fff'
+});
+
+var options = Ti.UI.createView({layout: 'vertical'});
+
+var emailFromLibrary = Ti.UI.createButton({title: 'Send photo to the cloud'});
+
+emailFromLibrary.addEventListener('click', function(e) {
+	Ti.Media.openPhotoGallery({
+		autoHide:   true,
+		mediaTypes: [Ti.Media.MEDIA_TYPE_PHOTO],
+		success:    function(e) {showPhoto(e); sendPiccyToCloud(e)}
+		});
+});
+
+var thePhoto = Ti.UI.createImageView({height: '30%', width: '30%'});
+
+function showPhoto(_args) {
+	thePhoto.setImage(_args.media);
+}
+
+function sendPiccyToCloud(_args) {
+    // first we need to write out a file of the piccy
+    var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'cloudThis.png');
+    file.write(_args.media);
+    // then send this file to the Cloud
+    alert(file.name);
+    forex.sendPiccy(file.name);
+    
+}
+var thePhoto = Ti.UI.createImageView({height: '30%', width: '30%'});
+
+
+
+options.add(emailFromLibrary);
+options.add(thePhoto);
+win1.add(options);
+win1.open();
---
+++ Resources/cloud.js
@@ -0,0 +1,38 @@
+var Cloud = require('Ti.Cloud');
+
+//login as the cloud user....
+function init(_args) {
+	if (!Cloud.sessionId) {
+		Cloud.Users.login({
+		    login: 'pic',
+		    password: 'piccy'
+		}, function (e) {
+		    if (e.success) {
+		        _args.success({user : e.users[0]});
+		    } else {
+		        _args.error({error: e.error});
+		    }
+		});
+	}
+};
+exports.init = init;
+
+function sendPiccy(_args) {
+	// create a new photo
+
+	Cloud.Photos.create({
+	    photo: Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory+'/'+_args)
+	}, function (e) {
+	    if (e.success) {
+	        var photo = e.photos[0];
+	        alert('Success:\n' +
+	            'id: ' + photo.id );
+	    } else {
+	        alert('Error:\\n' +
+	            ((e.error && e.message) || JSON.stringify(e)));
+	    }
+	    // release the file
+	    _args = null;
+	});
+}
+exports.sendPiccy = sendPiccy;
